615b710f35d457f8e8487d89be70c332a39563bf
goliaph <goliaph47@gmail.com>
2022-04-19 18:55:52 +0500

    initials

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Prognoz from "./components/Prognoz";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="header">Прогноз на 5 дней</h1>
       </header>
+      <Prognoz />
     </div>
   );
 }
---
+++ src/components/Prognoz.js
@@ -0,0 +1,74 @@
+import prognozCSS from "../styles/prognoz.module.css";
+import cloudwithrain from "../cloudwithrain.svg";
+import cloudwithrainandstorm from "../cloudwithstorm.svg";
+import moon from "../moon.svg";
+import moonandcloud from "../moonAndCloud.svg";
+import React from "react";
+
+const Budni = [
+  {
+    day: "Вчера",
+    date: "06.06",
+    weather: cloudwithrain,
+    weatherNight: moonandcloud,
+    wind: "3.1 m/c",
+  },
+  {
+    day: "Сегодня",
+    date: "07.06",
+    weather: cloudwithrain,
+    weatherNight: moon,
+    wind: "2.9 m/c",
+  },
+  {
+    day: "Завтра",
+    date: "08.06",
+    weather: cloudwithrainandstorm,
+    weatherNight: moonandcloud,
+    wind: "3.6 m/c",
+  },
+  {
+    day: "Ср",
+    date: "09.06",
+    weather: cloudwithrain,
+    weatherNight: moon,
+    wind: "3.1 m/c",
+  },
+  {
+    day: "Чт",
+    date: "10.06",
+    weather: cloudwithrain,
+    weatherNight: moon,
+    wind: "3.6 m/c",
+  },
+];
+
+function Prognoz() {
+  return (
+    <>
+      <div className={prognozCSS.mainContainer}>
+        {Budni.map((bud) => (
+          <div className={prognozCSS.eachDay} key={bud.day}>
+            <p className={prognozCSS.bigFont}>{bud.day}</p>
+            <p className={prognozCSS.smallFont}>{bud.date}</p>
+            <img
+              src={bud.weather}
+              className={prognozCSS.images}
+              alt="weather"
+            />
+            <div className={prognozCSS.random}></div>
+            <div className={prognozCSS.random}></div>
+            <img
+              src={bud.weatherNight}
+              className={prognozCSS.images}
+              alt="weatherNight"
+            />
+            <p className={prognozCSS.bigFont}>{bud.wind}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Prognoz;
