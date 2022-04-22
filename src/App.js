815db3a55b92d06b9291de850ebde73e2af364c6
sabrina34352 <s.babakulova@student.inha.uz>
2022-04-22 17:56:28 +0500

    changes to organization of folders

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import Forecast from "./components/Forecast";
+import { days } from "./assets/days";
 import "./App.css";
+import React, { useEffect } from "react";
 
 function App() {
+  const temperatureNight = [];
+  const temperatureDay = [];
+  useEffect(() => {
+    for (let i = 0; i < days.length; i++) {
+      temperatureDay.push(days[i].dayTemp);
+      temperatureNight.push(days[i].nightTemp);
+    }
+  }, []);
   return (
-    <>
     <div className="App">
       <header className="App-header">
         <h1 className="header">Прогноз на 5 дней</h1>
       </header>
-      <Forecast />
+      <Forecast
+        data={days}
+        temperatureNight={temperatureNight}
+        temperatureDay={temperatureDay}
+      />
     </div>
-    </>
   );
 }
 
---
+++ src/assets/days.js
@@ -0,0 +1,56 @@
+import cloudwithrain from "./cloudwithrain.svg";
+import cloudwithrainandstorm from "./cloudwithstorm.svg";
+import moon from "./moonReal.svg";
+import moonandcloud from "./moonAndClouds.svg";
+
+// const temperatureNight = [13, 14, 14, 13, 13, 14];
+// const temperatureDay = [23, 23, 24, 24, 22, 24];
+
+export const days = [
+  {
+    day: "Вчера",
+    date: "06.06",
+    weather: cloudwithrain,
+    weatherNight: moonandcloud,
+    wind: "3.1 m/c",
+    dayTemp: 23,
+    nightTemp: 13,
+  },
+  {
+    day: "Сегодня",
+    date: "07.06",
+    weather: cloudwithrain,
+    weatherNight: moon,
+    wind: "2.9 m/c",
+    dayTemp: 23,
+    nightTemp: 14,
+  },
+  {
+    day: "Завтра",
+    date: "08.06",
+    weather: cloudwithrainandstorm,
+    weatherNight: moonandcloud,
+    wind: "3.6 m/c",
+    dayTemp: 24,
+    nightTemp: 13,
+  },
+  {
+    day: "Ср",
+    date: "09.06",
+    weather: cloudwithrain,
+    weatherNight: moon,
+    wind: "3.1 m/c",
+    dayTemp: 22,
+    nightTemp: 13,
+  },
+  {
+    day: "Чт",
+    date: "10.06",
+    weather: cloudwithrain,
+    weatherNight: moon,
+    wind: "3.6 m/c",
+    dayTemp: 24,
+    nightTemp: 14,
+  }
+  
+];
