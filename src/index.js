292ab71676b05250263ef43ac8c6b0ae8e757b01
Sabrina Babakulova <s.babakulova@student.inha.uz>
2022-04-25 14:07:37 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
   // <React.StrictMode>
+root.render(
   <App />
-  // </React.StrictMode>
 );
