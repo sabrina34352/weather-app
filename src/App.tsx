2202163e5e76148a87add1ecdab353146e99f9bb
sabrina34352 <s.babakulova@student.inha.uz>
2022-04-28 15:28:26 +0500

    getting rid of CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
 import Forecast from "./components/Forecast";
 import { days } from "./components/days";
-import "./App.css";
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const StyledApp = styled.div`
+  text-align: center;
+  background-color: #0f0e0e;
+  width: 800px;
+  height: 750px;
+  margin: 3em auto;
+  border-radius: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: white;
+  @media screen and (max-width: 810px) {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+`;
+const Header = styled.h1`
+  font-size: 30px;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #dddddd;
+  }
+  @media screen and (max-width: 810px) {
+  body {
+    background-color: #0f0e0e;
+  }
+}
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="header">Прогноз на 5 дней</h1>
-      </header>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <Header>Прогноз на 5 дней</Header>
 
-      <Forecast data={days} />
-    </div>
+        <Forecast data={days} />
+      </StyledApp>
+    </>
   );
 }
 
